f9d23181b64f9974573686909ccb97545e63e476
rldalex <roland.alexandrer@gmail.com>
2026-03-10 19:33:34 +0100

    chore: bump version to 0.0.65

--- custom_components/glass_cards/const.py
+++ custom_components/glass_cards/const.py
@@ -4,7 +4,7 @@ import hashlib
 import os
 
 DOMAIN = "glass_cards"
-VERSION = "0.0.64"
+VERSION = "0.0.65"
 
 JS_PATH = "/glass_cards/glass-cards.js"
 PANEL_JS_PATH = "/glass_cards/glass-cards-panel.js"
