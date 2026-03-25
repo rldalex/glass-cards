0dfe7555c67f6657251c9ce90ea3c08ab6d3eadb
rldalex <roland.alexandrer@gmail.com>
2026-03-25 11:28:59 +0100

    feat(panel): import all cards for live preview in config panel

--- src/panel.ts
+++ src/panel.ts
@@ -3,3 +3,15 @@
  * HA loads this as a JS module and renders <glass-config-panel>.
  */
 import '../cards/config-panel/src/index';
+
+// Card imports for live preview in config panel tabs
+import '../cards/light-card/src/index';
+import '../cards/climate-card/src/index';
+import '../cards/cover-card/src/index';
+import '../cards/fan-card/src/index';
+import '../cards/media-card/src/index';
+import '../cards/weather-card/src/index';
+import '../cards/presence-card/src/index';
+import '../cards/spotify-card/src/index';
+import '../cards/camera-carousel/src/index';
+import '../cards/title-card/src/index';
